811669bd9d9fa4856f08d54903a7c8daa4641144
shinpei5481 <crocs13579@gmail.com>
2021-10-23 15:46:06 +0900

    サニタイズ

--- base.php
+++ base.php
@@ -14,16 +14,23 @@ $message = array();
 $message_array = array();
 $success_message = null;
 $error_message =array();
+$clean = array();
 
 if(!empty($_POST['btn_submit'])){
     // 表示名の入力チェック
     if(empty($_POST['view_name'])){
         $error_message[] = '表示名を入力してください。';
+    } else {
+        $clean['view_name'] = htmlspecialchars($_POST['view_name'], ENT_QUOTES);
+        $clean['view_name'] = preg_replace( '/\\r\\n|\\n|\\r/', '', $clean['view_name']);
     }
 
     // メッセージの入力チェック
     if(empty($_POST['message'])){
         $error_message[] = 'ひと言メッセージを入力してください。';
+    } else {
+        $clean['message'] =htmlspecialchars($_POST['message'], ENT_QUOTES);
+        $clean['message'] = preg_replace( '/\\r\\n|\\n|\\r/', '<br>', $clean['message']);
     }
 
     if(empty($error_message)){
@@ -31,8 +38,8 @@ if(!empty($_POST['btn_submit'])){
         if($file_handle = fopen( FILENAME, "a")){
         // 書き込み日時を取得
         $now_date = date("Y-m-d H:i:s");
-        // 書き込むデータの作成
-        $data = "'".$_POST['view_name']."','".$_POST['message']."','".$now_date."'\n";
+        // 書き込むデータを作成
+        $data = "'".$clean['view_name']."','".$clean['message']."','".$now_date."'\n";
         // 書き込み
         fwrite($file_handle, $data);
         // ファイルを閉じる
